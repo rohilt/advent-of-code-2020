7ecf63702ac0b5e9d23e5cea66caee50a5bb1de5
Rohil Tuli <rohil.tuli@gmail.com>
2020-12-01 12:43:00 -0500

    Added in automation for fetching input files

---
+++ fetch_input.py
@@ -0,0 +1,17 @@
+import sys
+import requests
+
+if len(sys.argv) < 2:
+    print("Pass in the day value as a CLI argument")
+    exit()
+
+url = "https://adventofcode.com/2020/day/{}/input".format(sys.argv[1])
+cookie_key = None
+with open('COOKIE_KEY') as cookie:
+    cookie_key = cookie.read()
+    cookie.close()
+
+r = requests.get(url, headers={'Cookie': cookie_key})
+with open('day{}.in'.format(sys.argv[1]), "x") as f:
+    f.write(r.text)
+    f.close()
