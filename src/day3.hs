f7fcbfa0e4193496642f80d51c22797b5f281979
Rohil Tuli <rohil.tuli@gmail.com>
2020-12-03 00:46:14 -0500

    Finished Day 3

--- src/day3.hs
+++ src/day3.hs
@@ -3,16 +3,36 @@ import Data.List
 
 main :: IO()
 main = do
-  -- input <- readFile "input/dayX.in"
+  input <- readFile "input/day3.in"
   print $ part1 $ parseInput input
   print $ part2 $ parseInput input
-    where input = "Not yet available"
 
-parseInput :: String -> String
-parseInput input = input
+parseInput :: String -> [[Square]]
+parseInput input = map parseLine $ lines input
+  where parseLine line = map (\x -> if (x == '.') then Open else Tree) line
 
-part1 :: String -> String
-part1 _ = "Not yet implemented"
+part1 :: [[Square]] -> Int
+part1 grid = countTrees grid 3
 
-part2 :: String -> String
-part2 _ = "Not yet implemented"
+part2 :: [[Square]] -> Int
+part2 grid = product $ ((countTrees' grid 2):(map (countTrees grid) [1,3,5,7]))
+
+countTrees :: [[Square]] -> Int -> Int
+countTrees grid slope = length $ filter isTree squares
+  where
+    indices = map (\x -> mod x $ length (grid !! 0) ) [0,slope..(slope*(length grid)-1)]
+    squares = [(grid !! i) !! (indices !! i) | i <- [0..(length grid - 1)]]
+
+countTrees' :: [[Square]] -> Int -> Int
+countTrees' grid slope = length $ filter isTree squares
+  where
+    indices = map (\x -> mod x $ length (grid !! 0) ) [0..]
+    indices' = [0,slope..(length grid - 1)]
+    squares = [(grid !! (indices' !! i) ) !! (indices !! i) | i <- [0..((length indices') - 1)]]
+
+
+data Square = Open | Tree
+
+isTree :: Square -> Bool
+isTree Tree = True
+isTree Open = False
