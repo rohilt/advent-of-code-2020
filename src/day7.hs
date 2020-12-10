b05c7b44b8dfc6d7c79a1ca5c93540f54364d5e5
Rohil Tuli <rohil.tuli@gmail.com>
2020-12-09 21:32:43 -0500

    Finished Day 7, Part 1

--- src/day7.hs
+++ src/day7.hs
@@ -1,18 +1,35 @@
 import System.IO
 import Data.List
+import Data.Maybe
+import qualified Data.Set as Set
 
 main :: IO()
 main = do
-  -- input <- readFile "input/dayX.in"
+  input <- readFile "../input/day7.in"
   print $ part1 $ parseInput input
   print $ part2 $ parseInput input
-    where input = "Not yet available"
 
-parseInput :: String -> String
-parseInput input = input
+parseInput :: String -> [(String, String)]
+parseInput = concat . map fromJust . filter isJust . map parseLine . lines
 
-part1 :: String -> String
-part1 _ = "Not yet implemented"
+parseLine :: String -> Maybe [(String, String)]
+parseLine line
+  | (length $ wline) == 7 = Nothing
+  | otherwise = Just [(container, content) | content <- contents]
+  where
+    wline = words line
+    color = intercalate " " . take 2
+    container = color wline
+    contents = [color $ drop i $ wline | i <- [5,9..(length wline - 1)] ]
 
-part2 :: String -> String
+part1 :: [(String, String)] -> Int
+part1 edges = Set.size $ dfs edges "shiny gold"
+
+part2 :: [(String, String)] -> String
 part2 _ = "Not yet implemented"
+
+dfs :: [(String, String)] -> String -> Set.Set String
+dfs edges s = Set.union (Set.fromList directContainers) indirectContainers
+  where
+    directContainers = map fst $ filter (\(_, c) -> c == s) edges
+    indirectContainers = foldl Set.union Set.empty $ map (dfs edges) directContainers
