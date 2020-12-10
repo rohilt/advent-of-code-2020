09e81b082ff33aa38628bb68d38a68c5b37e57d3
Rohil Tuli <rohil.tuli@gmail.com>
2020-12-10 09:32:12 -0500

    Finished Day 7, Part 2

--- src/day7.hs
+++ src/day7.hs
@@ -3,33 +3,50 @@ import Data.List
 import Data.Maybe
 import qualified Data.Set as Set
 
+type Rule = (String, String, Int)
+
 main :: IO()
 main = do
   input <- readFile "../input/day7.in"
   print $ part1 $ parseInput input
   print $ part2 $ parseInput input
 
-parseInput :: String -> [(String, String)]
+parseInput :: String -> [Rule]
 parseInput = concat . map fromJust . filter isJust . map parseLine . lines
 
-parseLine :: String -> Maybe [(String, String)]
+part1 :: [Rule] -> Int
+part1 rules = Set.size $ dfs rules "shiny gold"
+
+part2 :: [Rule] -> Int
+part2 rules = (getNumBags rules "shiny gold") - 1
+
+parseLine :: String -> Maybe [Rule]
 parseLine line
   | (length $ wline) == 7 = Nothing
-  | otherwise = Just [(container, content) | content <- contents]
+  | otherwise = Just [(container, content, num) | (content, num) <- contents]
   where
     wline = words line
     color = intercalate " " . take 2
     container = color wline
-    contents = [color $ drop i $ wline | i <- [5,9..(length wline - 1)] ]
+    contents = [(color $ drop i wline, read $ head $ drop (i-1) wline :: Int) | i <- [5,9..(length wline - 1)] ]
 
-part1 :: [(String, String)] -> Int
-part1 edges = Set.size $ dfs edges "shiny gold"
-
-part2 :: [(String, String)] -> String
-part2 _ = "Not yet implemented"
-
-dfs :: [(String, String)] -> String -> Set.Set String
-dfs edges s = Set.union (Set.fromList directContainers) indirectContainers
+dfs :: [Rule] -> String -> Set.Set String
+dfs rules s = Set.union (Set.fromList directContainers) indirectContainers
   where
-    directContainers = map fst $ filter (\(_, c) -> c == s) edges
-    indirectContainers = foldl Set.union Set.empty $ map (dfs edges) directContainers
+    directContainers = map getColor $ filter (\r -> (getColor2 r) == s) rules
+    indirectContainers = foldl Set.union Set.empty $ map (dfs rules) directContainers
+
+getNumBags :: [Rule] -> String -> Int
+getNumBags rules s = (+1) $ sum $ map recurseRule matchingRules
+  where
+    matchingRules = filter (\r -> (getColor r) == s) rules
+    recurseRule r = (getNum r) * ( (getNumBags rules) $ getColor2 r)
+
+getColor :: Rule -> String
+getColor (c, _, _) = c
+
+getColor2 :: Rule -> String
+getColor2 (_, c, _) = c
+
+getNum :: Rule -> Int
+getNum (_, _, n) = n
