d3ee351a42f4705a7d4d5a74afd34734437a2f83
Rohil Tuli <rohil.tuli@gmail.com>
2020-12-12 23:28:09 -0500

    Finished Day 9, Part 1

--- src/day9.hs
+++ src/day9.hs
@@ -1,18 +1,20 @@
 import System.IO
 import Data.List
+import Data.Maybe
 
 main :: IO()
 main = do
-  -- input <- readFile "input/dayX.in"
+  input <- readFile "../input/day9.in"
   print $ part1 $ parseInput input
   print $ part2 $ parseInput input
-    where input = "Not yet available"
 
-parseInput :: String -> String
-parseInput input = input
+parseInput :: String -> [Int]
+parseInput = map read . lines
 
-part1 :: String -> String
-part1 _ = "Not yet implemented"
+part1 :: [Int] -> Int
+part1 xs = fromJust $ head $ filter isJust [if (elem (xs !! i) (prevSum i)) then Nothing else Just (xs !! i) | i <- [25..(length xs - 1)] ]
+  where
+    prevSum i = [x + y | x <- take 25 $ drop (i-25) xs, y <- take 25 $ drop (i-25) xs]
 
-part2 :: String -> String
+part2 :: [Int] -> String
 part2 _ = "Not yet implemented"
