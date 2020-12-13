22f9c9ba13e1c38f19686a6e9939371a169f3451
Rohil Tuli <rohil.tuli@gmail.com>
2020-12-13 12:34:40 -0500

    Wrote solution for Day 10, Part 2, but it is too slow

--- src/day10.hs
+++ src/day10.hs
@@ -1,11 +1,14 @@
 import System.IO
 import Data.List
+import Data.Maybe
+import qualified Data.Map as Map
 
 main :: IO()
 main = do
   input <- readFile "../input/day10.in"
   print $ part1 $ parseInput input
   print $ part2 $ parseInput input
+  print $ differences $ parseInput input
 
 parseInput :: String -> [Int]
 parseInput = map read . lines
@@ -13,9 +16,17 @@ parseInput = map read . lines
 part1 :: [Int] -> Int
 part1 joltages = ((+1) $ numOnes joltages) * ((+1) $ numThrees joltages)
   where
-    differences joltages = [ (sort joltages !! (i+1)) - (sort joltages !! i) | i <- [0..length joltages - 2] ]
     numOnes = length . filter (==1) . differences
     numThrees = length . filter (==3) . differences
 
-part2 :: [Int] -> String
-part2 _ = "Not yet implemented"
+part2 :: [Int] -> Int
+part2 = countNumWays . differences . (0:)
+
+differences :: [Int] -> [Int]
+differences joltages = [ (sort joltages !! (i+1)) - (sort joltages !! i) | i <- [0..length joltages - 2] ]
+
+countNumWays :: [Int] -> Int
+countNumWays (x:y:xs)
+  | x + y <= 3 = countNumWays ((x+y):xs) + countNumWays (y:xs)
+  | otherwise = countNumWays (y:xs)
+countNumWays _ = 1
