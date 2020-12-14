f8bf6decacc56c540eaef6038839a8ea0a62b882
Rohil Tuli <rohil.tuli@gmail.com>
2020-12-13 22:35:06 -0500

    Finished Day 12, Part 2

--- src/day12.hs
+++ src/day12.hs
@@ -4,6 +4,7 @@ import Data.List
 data Instruction = North Int | South Int | East Int | West Int | TurnLeft | TurnRight | TurnAround | Forward Int
 data Direction = NorthDir | SouthDir | EastDir | WestDir
 type ShipPosition = ((Int, Int), Direction)
+type ShipPosition' = ((Int, Int), (Int, Int))
 
 main :: IO()
 main = do
@@ -31,30 +32,41 @@ parseInstruction (x:y)
 
 part1 :: [Instruction] -> Int
 part1 = manhattanDistance . fst . foldl interpretInstruction ((0, 0), EastDir)
-  where
-    manhattanDistance (x, y) = abs x + abs y
 
-part2 :: [Instruction] -> String
-part2 _ = "Not yet implemented"
+part2 :: [Instruction] -> Int
+part2 = manhattanDistance . fst . foldl interpretInstruction' ((0, 0), (10, 1))
 
 interpretInstruction :: ShipPosition -> Instruction -> ShipPosition
 interpretInstruction ((x, y), d) (North i) = ((x, y+i), d)
 interpretInstruction ((x, y), d) (South i) = ((x, y-i), d)
 interpretInstruction ((x, y), d) (East i) = ((x+i, y), d)
 interpretInstruction ((x, y), d) (West i) = ((x-i, y), d)
-interpretInstruction ((x, y), NorthDir) (TurnAround) = ((x, y), SouthDir)
-interpretInstruction ((x, y), SouthDir) (TurnAround) = ((x, y), NorthDir)
-interpretInstruction ((x, y), EastDir) (TurnAround) = ((x, y), WestDir)
-interpretInstruction ((x, y), WestDir) (TurnAround) = ((x, y), EastDir)
-interpretInstruction ((x, y), NorthDir) (TurnLeft) = ((x, y), WestDir)
-interpretInstruction ((x, y), SouthDir) (TurnLeft) = ((x, y), EastDir)
-interpretInstruction ((x, y), EastDir) (TurnLeft) = ((x, y), NorthDir)
-interpretInstruction ((x, y), WestDir) (TurnLeft) = ((x, y), SouthDir)
-interpretInstruction ((x, y), NorthDir) (TurnRight) = ((x, y), EastDir)
-interpretInstruction ((x, y), SouthDir) (TurnRight) = ((x, y), WestDir)
-interpretInstruction ((x, y), EastDir) (TurnRight) = ((x, y), SouthDir)
-interpretInstruction ((x, y), WestDir) (TurnRight) = ((x, y), NorthDir)
+interpretInstruction (p, NorthDir) (TurnAround) = (p, SouthDir)
+interpretInstruction (p, SouthDir) (TurnAround) = (p, NorthDir)
+interpretInstruction (p, EastDir) (TurnAround) = (p, WestDir)
+interpretInstruction (p, WestDir) (TurnAround) = (p, EastDir)
+interpretInstruction (p, NorthDir) (TurnLeft) = (p, WestDir)
+interpretInstruction (p, SouthDir) (TurnLeft) = (p, EastDir)
+interpretInstruction (p, EastDir) (TurnLeft) = (p, NorthDir)
+interpretInstruction (p, WestDir) (TurnLeft) = (p, SouthDir)
+interpretInstruction (p, NorthDir) (TurnRight) = (p, EastDir)
+interpretInstruction (p, SouthDir) (TurnRight) = (p, WestDir)
+interpretInstruction (p, EastDir) (TurnRight) = (p, SouthDir)
+interpretInstruction (p, WestDir) (TurnRight) = (p, NorthDir)
 interpretInstruction ((x, y), NorthDir) (Forward i) = ((x, y+i), NorthDir)
 interpretInstruction ((x, y), SouthDir) (Forward i) = ((x, y-i), SouthDir)
 interpretInstruction ((x, y), EastDir) (Forward i) = ((x+i, y), EastDir)
 interpretInstruction ((x, y), WestDir) (Forward i) = ((x-i, y), WestDir)
+
+interpretInstruction' :: ShipPosition' -> Instruction -> ShipPosition'
+interpretInstruction' (p, (x, y)) (North i) = (p, (x, y+i))
+interpretInstruction' (p, (x, y)) (South i) = (p, (x, y-i))
+interpretInstruction' (p, (x, y)) (East i) = (p, (x+i, y))
+interpretInstruction' (p, (x, y)) (West i) = (p, (x-i, y))
+interpretInstruction' (p, (x, y)) (TurnAround) = (p, (-x, -y))
+interpretInstruction' (p, (x, y)) (TurnLeft) = (p, (-y, x))
+interpretInstruction' (p, (x, y)) (TurnRight) = (p, (y, -x))
+interpretInstruction' ((x, y), (x', y')) (Forward i) = ((x + (x'*i), y + (y'*i)), (x', y'))
+
+manhattanDistance :: (Int, Int) -> Int
+manhattanDistance (x, y) = abs x + abs y
