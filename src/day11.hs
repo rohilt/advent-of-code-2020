b3aa1550ce1326b6d6368ddccb095f7fe61833e2
Rohil Tuli <rohil.tuli@gmail.com>
2020-12-13 20:29:23 -0500

    Finished Day 11, Part 2

--- src/day11.hs
+++ src/day11.hs
@@ -53,17 +53,21 @@ transformGrid' grid = grid // [ (i, (grid ! i) // [ (j, newValue (i, j) ) | j <-
   where
     numRows = snd $ bounds grid
     numCols = snd $ bounds (grid ! 1)
-    north (i, j) = if (i > 1 && ((grid ! (i-1)) ! j) == Occupied) then 1 else 0
-    south (i, j) = if (i < numRows && ((grid ! (i+1)) ! j) == Occupied) then 1 else 0
-    east (i, j) = if (j > 1 && ((grid ! i) ! (j-1)) == Occupied) then 1 else 0
-    west (i, j) = if (j < numCols && ((grid ! i) ! (j+1)) == Occupied) then 1 else 0
-    northeast (i, j) = if (i > 1 && j > 1 && ((grid ! (i-1)) ! (j-1)) == Occupied) then 1 else 0
-    northwest (i, j) = if (i > 1 && j < numCols && ((grid ! (i-1)) ! (j+1)) == Occupied) then 1 else 0
-    southeast (i, j) = if (i < numRows && j > 1 && ((grid ! (i+1)) ! (j-1)) == Occupied) then 1 else 0
-    southwest (i, j) = if (i < numRows && j < numCols && ((grid ! (i+1)) ! (j+1)) == Occupied) then 1 else 0
+    north (i, j) = [ ((grid ! i') ! j') | (i', j') <- (zip (reverse [1..i-1]) (repeat j))]
+    south (i, j) = [ ((grid ! i') ! j') | (i', j') <- (zip [i+1..numRows] (repeat j))]
+    east (i, j) = [ ((grid ! i') ! j') | (i', j') <- (zip (repeat i) [j+1..numCols])]
+    west (i, j) = [ ((grid ! i') ! j') | (i', j') <- (zip (repeat i) (reverse [1..j-1]))]
+    northeast (i, j) = [ ((grid ! i') ! j') | (i', j') <- (zip (reverse [1..i-1]) [j+1..numCols])]
+    northwest (i, j) = [ ((grid ! i') ! j') | (i', j') <- (zip (reverse [1..i-1]) (reverse [1..j-1]))]
+    southeast (i, j) = [ ((grid ! i') ! j') | (i', j') <- (zip [i+1..numRows] [j+1..numCols])]
+    southwest (i, j) = [ ((grid ! i') ! j') | (i', j') <- (zip [i+1..numRows] (reverse [1..j-1]))]
     directions = [north, south, east, west, northeast, northwest, southeast, southwest]
-    occupiedNeighbors (i, j) = (>= 4) $ sum $ map (\f -> f (i, j)) directions
-    noOccupiedNeighbors (i, j) = (== 0) $ sum $ map (\f -> f (i, j)) directions
+    interpretSeat Occupied = 1
+    interpretSeat Empty = 0
+    interpretDirection' = interpretSeat . head . dropWhile (== Floor)
+    interpretDirection x = if ((== 0) $ length $ dropWhile (== Floor) x) then 0 else interpretDirection' x
+    occupiedNeighbors (i, j) = (>= 5) $ sum $ map (\f -> interpretDirection $ f (i, j)) directions
+    noOccupiedNeighbors (i, j) = (== 0) $ sum $ map (\f -> interpretDirection $ f (i, j)) directions
     newValue (i, j)
       | ((grid ! i) ! j) == Floor = Floor
       | ((grid ! i) ! j) == Occupied = if (occupiedNeighbors (i, j)) then Empty else Occupied
