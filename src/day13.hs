b737711e7fac1225b70ba40995a6ca10f83d4847
Rohil Tuli <rohil.tuli@gmail.com>
2020-12-13 22:58:59 -0500

    Finished Day 13, Part 1

--- src/day13.hs
+++ src/day13.hs
@@ -1,18 +1,31 @@
 import System.IO
 import Data.List
+import Data.Maybe
+
+type BusId = Maybe Int
+type Information = (Int, [BusId])
 
 main :: IO()
 main = do
-  -- input <- readFile "input/dayX.in"
+  input <- readFile "../input/day13.in"
   print $ part1 $ parseInput input
   print $ part2 $ parseInput input
-    where input = "Not yet available"
 
-parseInput :: String -> String
-parseInput input = input
+parseInput :: String -> Information
+parseInput = formatInput . words
+  where
+    formatInput (x:y:[]) = (read x, readIds y)
+    readIds "" = []
+    readIds (',':y) = readIds y
+    readIds y = (readId $ takeWhile (/= ',') y):(readIds $ dropWhile (/= ',') y)
+    readId "x" = Nothing
+    readId x = Just (read x)
 
-part1 :: String -> String
-part1 _ = "Not yet implemented"
+part1 :: Information -> Int
+part1 (minTime, busIds) = multiplyTuple $ head $ sortOn snd $ map (waitTime . fromJust) $ filter isJust busIds
+  where
+    waitTime busId = (busId, busId - (minTime `mod` busId))
+    multiplyTuple (x, y) = x*y
 
-part2 :: String -> String
+part2 :: Information -> String
 part2 _ = "Not yet implemented"
