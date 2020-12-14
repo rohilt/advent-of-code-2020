496c8ac16bf562c5d78888e994b04e3202d9ce3c
Rohil Tuli <rohil.tuli@gmail.com>
2020-12-13 20:05:24 -0500

    Adjusted Day 11 to use Data.Array instead, also restructured to start Part 2

--- src/day11.hs
+++ src/day11.hs
@@ -4,7 +4,7 @@ import Data.Array
 
 data GridElement = Floor | Empty | Occupied
   deriving (Eq)
-type Grid = [[GridElement]]
+type Grid = Array Int (Array Int GridElement)
 
 main :: IO()
 main = do
@@ -13,41 +13,64 @@ main = do
   print $ part2 $ parseInput input
 
 parseInput :: String -> Grid
-parseInput input = map (map interpretChar) $ lines input
+parseInput input = listArray (1, numRows) $ map (listArray (1, numCols) . map interpretChar) $ lines input
   where
+    numRows = length $ lines input
+    numCols = length $ ((lines input) !! 0)
     interpretChar '.' = Floor
     interpretChar 'L' = Empty
     interpretChar '#' = Occupied
 
 part1 :: Grid -> Int
-part1 = countOccupied . getLastGrid
+part1 = countOccupied . getLastGrid transformGrid
 
-part2 :: Grid -> String
-part2 _ = "Not yet implemented"
+part2 :: Grid -> Int
+part2 = countOccupied . getLastGrid transformGrid'
 
 transformGrid :: Grid -> Grid
-transformGrid grid = [ [ newValue (i, j) | j <- [0..(numCols - 1)]] | i <- [0..(numRows - 1)]]
+transformGrid grid = grid // [ (i, (grid ! i) // [ (j, newValue (i, j) ) | j <- [1..numCols] ] ) | i <- [1..numRows] ]
   where
-    numRows = length grid
-    numCols = length (grid !! 0)
-    north (i, j) = if (i > 0 && ((grid !! (i-1)) !! j) == Occupied) then 1 else 0
-    south (i, j) = if (i < (numRows-1) && ((grid !! (i+1)) !! j) == Occupied) then 1 else 0
-    east (i, j) = if (j > 0 && ((grid !! i) !! (j-1)) == Occupied) then 1 else 0
-    west (i, j) = if (j < (numCols-1) && ((grid !! i) !! (j+1)) == Occupied) then 1 else 0
-    northeast (i, j) = if (i > 0 && j > 0 && ((grid !! (i-1)) !! (j-1)) == Occupied) then 1 else 0
-    northwest (i, j) = if (i > 0 && j < (numCols-1) && ((grid !! (i-1)) !! (j+1)) == Occupied) then 1 else 0
-    southeast (i, j) = if (i < (numRows-1) && j > 0 && ((grid !! (i+1)) !! (j-1)) == Occupied) then 1 else 0
-    southwest (i, j) = if (i < (numRows-1) && j < (numCols-1) && ((grid !! (i+1)) !! (j+1)) == Occupied) then 1 else 0
+    numRows = snd $ bounds grid
+    numCols = snd $ bounds (grid ! 1)
+    north (i, j) = if (i > 1 && ((grid ! (i-1)) ! j) == Occupied) then 1 else 0
+    south (i, j) = if (i < numRows && ((grid ! (i+1)) ! j) == Occupied) then 1 else 0
+    east (i, j) = if (j > 1 && ((grid ! i) ! (j-1)) == Occupied) then 1 else 0
+    west (i, j) = if (j < numCols && ((grid ! i) ! (j+1)) == Occupied) then 1 else 0
+    northeast (i, j) = if (i > 1 && j > 1 && ((grid ! (i-1)) ! (j-1)) == Occupied) then 1 else 0
+    northwest (i, j) = if (i > 1 && j < numCols && ((grid ! (i-1)) ! (j+1)) == Occupied) then 1 else 0
+    southeast (i, j) = if (i < numRows && j > 1 && ((grid ! (i+1)) ! (j-1)) == Occupied) then 1 else 0
+    southwest (i, j) = if (i < numRows && j < numCols && ((grid ! (i+1)) ! (j+1)) == Occupied) then 1 else 0
     directions = [north, south, east, west, northeast, northwest, southeast, southwest]
     occupiedNeighbors (i, j) = (>= 4) $ sum $ map (\f -> f (i, j)) directions
     noOccupiedNeighbors (i, j) = (== 0) $ sum $ map (\f -> f (i, j)) directions
     newValue (i, j)
-      | ((grid !! i) !! j) == Floor = Floor
-      | ((grid !! i) !! j) == Occupied = if (occupiedNeighbors (i, j)) then Empty else Occupied
-      | ((grid !! i) !! j) == Empty = if (noOccupiedNeighbors (i, j)) then Occupied else Empty
+      | ((grid ! i) ! j) == Floor = Floor
+      | ((grid ! i) ! j) == Occupied = if (occupiedNeighbors (i, j)) then Empty else Occupied
+      | ((grid ! i) ! j) == Empty = if (noOccupiedNeighbors (i, j)) then Occupied else Empty
 
-getLastGrid :: Grid -> Grid
-getLastGrid = getLast . iterate transformGrid
+transformGrid' :: Grid -> Grid
+transformGrid' grid = grid // [ (i, (grid ! i) // [ (j, newValue (i, j) ) | j <- [1..numCols] ] ) | i <- [1..numRows] ]
+  where
+    numRows = snd $ bounds grid
+    numCols = snd $ bounds (grid ! 1)
+    north (i, j) = if (i > 1 && ((grid ! (i-1)) ! j) == Occupied) then 1 else 0
+    south (i, j) = if (i < numRows && ((grid ! (i+1)) ! j) == Occupied) then 1 else 0
+    east (i, j) = if (j > 1 && ((grid ! i) ! (j-1)) == Occupied) then 1 else 0
+    west (i, j) = if (j < numCols && ((grid ! i) ! (j+1)) == Occupied) then 1 else 0
+    northeast (i, j) = if (i > 1 && j > 1 && ((grid ! (i-1)) ! (j-1)) == Occupied) then 1 else 0
+    northwest (i, j) = if (i > 1 && j < numCols && ((grid ! (i-1)) ! (j+1)) == Occupied) then 1 else 0
+    southeast (i, j) = if (i < numRows && j > 1 && ((grid ! (i+1)) ! (j-1)) == Occupied) then 1 else 0
+    southwest (i, j) = if (i < numRows && j < numCols && ((grid ! (i+1)) ! (j+1)) == Occupied) then 1 else 0
+    directions = [north, south, east, west, northeast, northwest, southeast, southwest]
+    occupiedNeighbors (i, j) = (>= 4) $ sum $ map (\f -> f (i, j)) directions
+    noOccupiedNeighbors (i, j) = (== 0) $ sum $ map (\f -> f (i, j)) directions
+    newValue (i, j)
+      | ((grid ! i) ! j) == Floor = Floor
+      | ((grid ! i) ! j) == Occupied = if (occupiedNeighbors (i, j)) then Empty else Occupied
+      | ((grid ! i) ! j) == Empty = if (noOccupiedNeighbors (i, j)) then Occupied else Empty
+
+getLastGrid :: (Grid -> Grid) -> Grid -> Grid
+getLastGrid transform = getLast . iterate transform
   where
     getLast (x:y:xs)
       | (x == y) = x
@@ -55,7 +78,7 @@ getLastGrid = getLast . iterate transformGrid
     getLast _ = error "This shouldn't happen"
 
 countOccupied :: Grid -> Int
-countOccupied = sum . map (sum . map isOccupied)
+countOccupied = sum . map (sum . map isOccupied . elems) . elems
   where
     isOccupied Occupied = 1
     isOccupied _ = 0
