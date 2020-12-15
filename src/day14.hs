8e84fe75adf4012474008d51f3e6da7e4853e4b6
Rohil Tuli <rohil.tuli@gmail.com>
2020-12-15 14:59:11 -0500

    Working on Day 14, Part 1

--- src/day14.hs
+++ src/day14.hs
@@ -1,18 +1,35 @@
 import System.IO
 import Data.List
+import qualified Data.Map as Map
+
+type Mask = [(Int, Bool)]
+data ProgramInstruction = SetMask Mask | SetMemory (Int, Int)
+  deriving (Show)
+type State = (Map.Map Int Int, Mask)
 
 main :: IO()
 main = do
-  -- input <- readFile "input/dayX.in"
+  input <- readFile "../input/day14.in"
   print $ part1 $ parseInput input
   print $ part2 $ parseInput input
-    where input = "Not yet available"
 
-parseInput :: String -> String
-parseInput input = input
+parseInput :: String -> [ProgramInstruction]
+parseInput = map (formatInstr . words) . lines
+  where
+    formatInstr ("mask":_:mask:[]) = SetMask $ formatMask (reverse mask) 0
+    formatInstr (('m':'e':'m':'[':addr):_:num:[]) = SetMemory (read $ init addr, read num)
+    formatMask ('X':xs) i = formatMask xs (i+1)
+    formatMask ('1':xs) i = (i,True):(formatMask xs (i+1))
+    formatMask ('0':xs) i = (i,False):(formatMask xs (i+1))
+    formatMask [] _ = []
 
-part1 :: String -> String
-part1 _ = "Not yet implemented"
+part1 :: [ProgramInstruction] -> Int
+part1 = sum . map snd . Map.toList . fst . finalState
+  where
+    finalState = foldl interpretInstruction (Map.empty, [])
 
-part2 :: String -> String
+part2 :: [ProgramInstruction] -> String
 part2 _ = "Not yet implemented"
+
+interpretInstruction :: State -> ProgramInstruction -> State
+interpretInstruction s _ = s
