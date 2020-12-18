0aa2ba8808f5b18cc6cb6cf96c398688f376167f
Rohil Tuli <rohil.tuli@gmail.com>
2020-12-18 16:12:07 -0500

    Finished Day 15, Part 1

--- src/day15.hs
+++ src/day15.hs
@@ -1,5 +1,9 @@
 import System.IO
 import Data.List
+import qualified Data.Map as Map
+
+type GameMemory = Map.Map Int Int
+type Turn = (Int, Int, GameMemory)
 
 main :: IO()
 main = do
@@ -15,8 +19,22 @@ splitAtComma input
   | (not $ elem ',' input) = [read input]
   | otherwise = (read $ takeWhile (/= ',') input):(splitAtComma $ tail $ dropWhile (/= ',') input)
 
-part1 :: [Int] -> [Int]
-part1 i = i
+part1 :: [Int] -> Int
+part1 nums = ((init nums) ++ (map (\(x,_,_) -> x) $ iterate numSpokenAt (last nums, length nums + 1, generateInitialMemory $ init nums))) !! (2020-1)
 
 part2 :: [Int] -> String
 part2 _ = "Not yet implemented"
+
+generateInitialMemory :: [Int] -> GameMemory
+generateInitialMemory xs = Map.fromList $ zip xs [1..]
+
+numSpokenAt :: Turn -> Turn
+numSpokenAt (prevNum, turn, mem)
+  | Map.member prevNum mem = (turn - 1 - (mem Map.! prevNum), turn+1, newMem)
+  | otherwise = (0, turn+1, newMem)
+  where
+    newMem = Map.insert prevNum (turn-1) mem
+
+-- numSpokenAt :: Int -> [Int] -> GameMemory -> Int
+-- numSpokenAt n nums m
+  -- | length nums == n = head nums --reverse input, place new elements at front
