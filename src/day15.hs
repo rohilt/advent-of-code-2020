0c86626e2c8e80d586eb0df1358bff6fd7293b08
Rohil Tuli <rohil.tuli@gmail.com>
2020-12-18 16:35:17 -0500

    Finished Day 15, Part 2

--- src/day15.hs
+++ src/day15.hs
@@ -20,21 +20,18 @@ splitAtComma input
   | otherwise = (read $ takeWhile (/= ',') input):(splitAtComma $ tail $ dropWhile (/= ',') input)
 
 part1 :: [Int] -> Int
-part1 nums = ((init nums) ++ (map (\(x,_,_) -> x) $ iterate numSpokenAt (last nums, length nums + 1, generateInitialMemory $ init nums))) !! (2020-1)
+part1 = getNthNumber 2020
 
-part2 :: [Int] -> String
-part2 _ = "Not yet implemented"
+part2 :: [Int] -> Int
+part2 = getNthNumber 30000000
 
 generateInitialMemory :: [Int] -> GameMemory
 generateInitialMemory xs = Map.fromList $ zip xs [1..]
 
 numSpokenAt :: Turn -> Turn
-numSpokenAt (prevNum, turn, mem)
-  | Map.member prevNum mem = (turn - 1 - (mem Map.! prevNum), turn+1, newMem)
-  | otherwise = (0, turn+1, newMem)
+numSpokenAt (prevNum, turn, mem) = if Map.member prevNum mem then (turn - 1 - (mem Map.! prevNum), turn+1, newMem) else (0, turn+1, newMem)
   where
     newMem = Map.insert prevNum (turn-1) mem
 
--- numSpokenAt :: Int -> [Int] -> GameMemory -> Int
--- numSpokenAt n nums m
-  -- | length nums == n = head nums --reverse input, place new elements at front
+getNthNumber :: Int -> [Int] -> Int
+getNthNumber n nums = (\(x,_,_) -> x) $ (iterate numSpokenAt (last nums, length nums + 1, generateInitialMemory $ init nums)) !! (n - (length nums))
