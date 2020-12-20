f56581c5b44d6bdea2dab7792beec25b0d0c9072
Rohil Tuli <rohil.tuli@gmail.com>
2020-12-20 13:54:57 -0500

    Finished Day 19, Part 2

--- src/day19.hs
+++ src/day19.hs
@@ -34,12 +34,19 @@ part1 (rules, msgs) = length $ filter (\m -> hasSolution $ matchesRule rules m 0
   where
     hasSolution = (> 0) . length . filter (== (Just ""))
 
-part2 :: Input -> String
-part2 _ = "Not yet implemented"
+part2 :: Input -> Int
+part2 = part1 . adjustMap
+  where
+    adjust8 = Map.insert 8 $ R [[42],[42,8]]
+    adjust11 = Map.insert 11 $ R [[42,31],[42,11,31]]
+    adjustMap (map, msgs) = (adjust8 $ adjust11 $ map, msgs)
 
 matchesRule :: RuleMap -> [Maybe String] -> Int -> [Maybe String]
 matchesRule _ [] _ = []
 matchesRule m (Nothing:ys) i = (Nothing:ys')
+  where
+    ys' = matchesRule m ys i
+matchesRule m ((Just ""):ys) i = (Nothing:ys')
   where
     ys' = matchesRule m ys i
 matchesRule m ((Just (x:xs)):ys) i =
