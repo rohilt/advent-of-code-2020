38af9f3b7fd6adce6e8e334e9b2888a19a909d6d
Rohil Tuli <rohil.tuli@gmail.com>
2020-12-19 23:29:26 -0500

    Working on Day 18, Part 1

--- src/day18.hs
+++ src/day18.hs
@@ -1,18 +1,40 @@
 import System.IO
 import Data.List
+import Data.Maybe
+import Data.Char
+
+data Token = Addition | Multiplication | LeftP | RightP | Number Int
+type Expression = [Token]
+data StackFrame = SNumber Int | SFunction (Int -> Int)
+type Stack = [Maybe StackFrame]
 
 main :: IO()
 main = do
-  -- input <- readFile "input/dayX.in"
+  input <- readFile "../input/day18.in"
   print $ part1 $ parseInput input
   print $ part2 $ parseInput input
-    where input = "Not yet available"
 
-parseInput :: String -> String
-parseInput input = input
+parseInput :: String -> [Expression]
+parseInput = map parseInput' . lines
 
-part1 :: String -> String
-part1 _ = "Not yet implemented"
+parseInput' :: String -> Expression
+parseInput' (x:xs)
+  | x == ' ' = parseInput' xs
+  | x == '+' = Addition:(parseInput' xs)
+  | x == '*' = Multiplication:(parseInput' xs)
+  | x == '(' = LeftP:(parseInput' xs)
+  | x == ')' = LeftP:(parseInput' xs)
+  | otherwise = (Number (read $ takeWhile isDigit (x:xs))):(parseInput' $ dropWhile isDigit (x:xs))
+parseInput' [] = []
 
-part2 :: String -> String
+part1 :: [Expression] -> Int
+part1 = sum . map (interpretStackResult . head . foldl interpretExpression [Nothing])
+  where
+    interpretStackResult (Just (SNumber x)) = x
+    interpretStackResult _ = error "This shouldn't happen"
+
+part2 :: [Expression] -> String
 part2 _ = "Not yet implemented"
+
+interpretExpression :: Stack -> Token -> Stack
+interpretExpression s _ = s
