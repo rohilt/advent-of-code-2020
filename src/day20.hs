8b73bc49dd4da4693a3848e4f9a3ab139386706b
Rohil Tuli <rohil.tuli@gmail.com>
2020-12-20 16:26:22 -0500

    Finished Day 20, Part 1

--- src/day20.hs
+++ src/day20.hs
@@ -22,8 +22,22 @@ parseInput = foldl readTile Map.empty . splitOn "" . lines
     readTileContents (_:xs) = map (map readPixel) xs
     readTile i t = Map.insert (readTileId t) (readTileContents t) i
 
-part1 :: Input -> Input
-part1 = id
+part1 :: Input -> Int
+part1 = product . take 4 . map fst . sortOn snd . getMatchesById
 
 part2 :: Input -> String
 part2 _ = "Not yet implemented"
+
+getEdges :: ImageTile -> [[Pixel]]
+getEdges t = basicRows ++ (map reverse basicRows)
+  where
+    topRow = (t !! 0)
+    bottomRow = (t !! (length t - 1))
+    leftColumn = map (\t' -> (t' !! 0)) t
+    rightColumn = map (\t' -> (t' !! (length t' - 1))) t
+    basicRows = [topRow, bottomRow, leftColumn, rightColumn]
+
+getMatchesById :: Input -> [(Int, Int)]
+getMatchesById input = [ (k, length $ intersect allPossibleEdges $ getEdges (input Map.! k)) | k <- (Map.keys input) ]
+  where
+    allPossibleEdges = concat $ map (getEdges . snd) $ Map.toList input
