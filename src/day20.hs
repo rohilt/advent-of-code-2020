feca0c3d8e61e4adcd4d6971e1e415c25f4265fd
Rohil Tuli <rohil.tuli@gmail.com>
2020-12-20 15:48:04 -0500

    Working on Day 20, Part 1

--- src/day20.hs
+++ src/day20.hs
@@ -1,18 +1,29 @@
 import System.IO
 import Data.List
+import qualified Data.Map as Map
+import Helper.Parse
+
+type Pixel = Bool
+type ImageTile = [[Pixel]]
+type Input = Map.Map Int ImageTile
 
 main :: IO()
 main = do
-  -- input <- readFile "input/dayX.in"
+  input <- readFile "../input/day20.in"
   print $ part1 $ parseInput input
   print $ part2 $ parseInput input
-    where input = "Not yet available"
 
-parseInput :: String -> String
-parseInput input = input
+parseInput :: String -> Input
+parseInput = foldl readTile Map.empty . splitOn "" . lines
+  where
+    readTileId (x:_) = read $ init $ last $ words x
+    readPixel '#' = True
+    readPixel '.' = False
+    readTileContents (_:xs) = map (map readPixel) xs
+    readTile i t = Map.insert (readTileId t) (readTileContents t) i
 
-part1 :: String -> String
-part1 _ = "Not yet implemented"
+part1 :: Input -> Input
+part1 = id
 
-part2 :: String -> String
+part2 :: Input -> String
 part2 _ = "Not yet implemented"
