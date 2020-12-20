be147f9b1342fdbc4fb4fc4d2dfcbbe53328890b
Rohil Tuli <rohil.tuli@gmail.com>
2020-12-20 12:22:30 -0500

    Working on Day 19, Part 1

--- src/day19.hs
+++ src/day19.hs
@@ -1,18 +1,38 @@
 import System.IO
 import Data.List
+import Data.Char
+import qualified Data.Map as Map
+import Helper.Parse
+
+data Rule = C Char | R [[Int]]
+type RuleMap = Map.Map Int Rule
+type Input = (RuleMap, [String])
 
 main :: IO()
 main = do
-  -- input <- readFile "input/dayX.in"
+  input <- readFile "../input/day19.in"
   print $ part1 $ parseInput input
   print $ part2 $ parseInput input
-    where input = "Not yet available"
 
-parseInput :: String -> String
-parseInput input = input
+parseInput :: String -> Input
+parseInput = createInput . splitOn "" . lines
+  where
+    createInput (rules:msgs:[]) = (foldl parseRule Map.empty rules, msgs)
+    createInput _ = error "This shouldn't happen"
 
-part1 :: String -> String
-part1 _ = "Not yet implemented"
+parseRule :: RuleMap -> String -> RuleMap
+parseRule m s
+  | ((=='"') $ head $ last $ words s) = finalMap $ C $ head $ tail $ last $ words s
+  | otherwise = finalMap $ R $ map (map read) $ splitOn "|" $ tail $ words s
+  where
+    ruleNum = read $ takeWhile isDigit s
+    finalMap r = Map.insert ruleNum r m
 
-part2 :: String -> String
+part1 :: Input -> Int
+part1 (rules, msgs) = length $ filter (matchesRule rules 0) msgs
+
+part2 :: Input -> String
 part2 _ = "Not yet implemented"
+
+matchesRule :: RuleMap -> Int -> String -> Bool
+matchesRule _ _ _ = True
