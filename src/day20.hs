2fe4109eec3ab3bb19e8f7c36e5677af5a9b3fd1
Rohil Tuli <rohil.tuli@gmail.com>
2020-12-20 22:23:15 -0500

    Working on Day 20, Part 2

--- src/day20.hs
+++ src/day20.hs
@@ -31,13 +31,87 @@ part2 _ = "Not yet implemented"
 getEdges :: ImageTile -> [[Pixel]]
 getEdges t = basicRows ++ (map reverse basicRows)
   where
-    topRow = (t !! 0)
-    bottomRow = (t !! (length t - 1))
-    leftColumn = map (\t' -> (t' !! 0)) t
-    rightColumn = map (\t' -> (t' !! (length t' - 1))) t
-    basicRows = [topRow, bottomRow, leftColumn, rightColumn]
+    basicRows = map (\f -> f t) [topRow, bottomRow, leftColumn, rightColumn]
 
 getMatchesById :: Input -> [(Int, Int)]
 getMatchesById input = [ (k, length $ intersect allPossibleEdges $ getEdges (input Map.! k)) | k <- (Map.keys input) ]
   where
     allPossibleEdges = concat $ map (getEdges . snd) $ Map.toList input
+
+adjustTileInitialCorner :: Input -> Int -> [Pixel] -> Input
+adjustTileInitialCorner map i match = Map.insert i newTile map
+  where
+    tile = (map Map.! i)
+    newTile
+      | leftColumn tile == match = reflectVertical tile
+      | rightColumn tile == match = tile
+      | topRow tile == match = reflectHorizontal tile
+      | bottomRow tile == match = tile
+      | (reverse $ leftColumn tile) == match = rotate180 tile
+      | (reverse $ rightColumn tile) == match = reflectHorizontal tile
+      | (reverse $ topRow tile) == match = reflectUpDiagonal tile
+      | (reverse $ bottomRow tile) == match = rotate270 tile
+
+adjustTileMatchLeft :: Input -> Int -> [Pixel] -> Input -- TODO
+adjustTileMatchLeft map i match = Map.insert i newTile map
+  where
+    tile = (map Map.! i)
+    newTile
+      | leftColumn tile == match = reflectVertical tile
+      | rightColumn tile == match = tile
+      | topRow tile == match = reflectHorizontal tile
+      | bottomRow tile == match = tile
+      | (reverse $ leftColumn tile) == match = rotate180 tile
+      | (reverse $ rightColumn tile) == match = reflectHorizontal tile
+      | (reverse $ topRow tile) == match = reflectUpDiagonal tile
+      | (reverse $ bottomRow tile) == match = rotate270 tile
+
+adjustTileMatchUp :: Input -> Int -> [Pixel] -> Input -- TODO
+adjustTileMatchUp map i match = Map.insert i newTile map
+  where
+    tile = (map Map.! i)
+    newTile
+      | leftColumn tile == match = reflectVertical tile
+      | rightColumn tile == match = tile
+      | topRow tile == match = reflectHorizontal tile
+      | bottomRow tile == match = tile
+      | (reverse $ leftColumn tile) == match = rotate180 tile
+      | (reverse $ rightColumn tile) == match = reflectHorizontal tile
+      | (reverse $ topRow tile) == match = reflectUpDiagonal tile
+      | (reverse $ bottomRow tile) == match = rotate270 tile
+
+reflectHorizontal :: ImageTile -> ImageTile
+reflectHorizontal t = [ (t !! i) | i <- (reverse [0..(length t - 1)]) ]
+
+reflectVertical :: ImageTile -> ImageTile
+reflectVertical = map (\t' -> [ (t' !! i) | i <- (reverse [0..(length t' - 1)]) ])
+
+reflectUpDiagonal :: ImageTile -> ImageTile
+reflectUpDiagonal = reflectDownDiagonal . rotate180
+
+reflectDownDiagonal :: ImageTile -> ImageTile
+reflectDownDiagonal = transpose
+
+rotate0 :: ImageTile -> ImageTile
+rotate0 = id
+
+rotate90 :: ImageTile -> ImageTile
+rotate90 = reflectVertical . reflectDownDiagonal
+
+rotate180 :: ImageTile -> ImageTile
+rotate180 = reverse . map reverse
+
+rotate270 :: ImageTile -> ImageTile
+rotate270 = reflectHorizontal . reflectDownDiagonal
+
+topRow :: ImageTile -> [Pixel]
+topRow t = (t !! 0)
+
+bottomRow :: ImageTile -> [Pixel]
+bottomRow t = (t !! (length t - 1))
+
+leftColumn :: ImageTile -> [Pixel]
+leftColumn t = map (\t' -> (t' !! 0)) t
+
+rightColumn :: ImageTile -> [Pixel]
+rightColumn t = map (\t' -> (t' !! (length t' - 1))) t
