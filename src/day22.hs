c5d2b4492d9b2f478d4f8eb1536d733de6a13897
Rohil Tuli <rohil.tuli@gmail.com>
2020-12-25 22:34:41 -0500

    Finished Day 22, Part 1

--- src/day22.hs
+++ src/day22.hs
@@ -1,18 +1,38 @@
 import System.IO
 import Data.List
+import Helper.Parse
+
+type CardDeck = [Int]
+type Input = (CardDeck, CardDeck)
 
 main :: IO()
 main = do
-  -- input <- readFile "input/dayX.in"
+  input <- readFile "../input/day22.in"
   print $ part1 $ parseInput input
   print $ part2 $ parseInput input
-    where input = "Not yet available"
 
-parseInput :: String -> String
-parseInput input = input
+parseInput :: String -> Input
+parseInput = listToTuple . map (map read . tail) . splitOn "" . lines
+  where
+    listToTuple (a:b:[]) = (a,b)
 
-part1 :: String -> String
-part1 _ = "Not yet implemented"
+part1 :: Input -> Int
+part1 = getScore . winningDeck
 
-part2 :: String -> String
+part2 :: Input -> String
 part2 _ = "Not yet implemented"
+
+playRound :: Input -> Input
+playRound ((x:xs), (y:ys))
+  | x > y = (xs ++ [x,y], ys)
+  | x < y = (xs, ys ++ [y,x])
+  | otherwise = error "This shouldn't happen"
+
+winningDeck :: Input -> CardDeck
+winningDeck = takeWinningDeck . head . dropWhile (\(x,y) -> x /= [] && y /= []) . iterate playRound
+  where
+    takeWinningDeck ([], x) = x
+    takeWinningDeck (x, []) = x
+
+getScore :: CardDeck -> Int
+getScore cards = sum $ [ (length cards - i)*(cards !! i) | i <- [0..(length cards - 1)] ]
